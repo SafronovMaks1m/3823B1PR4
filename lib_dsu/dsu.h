516f5ebb9e9aae3cf60cec0ddfe7b1acb35e093c
SafronovMaks1m <safranov01@list.ru>
2025-02-10 11:24:10 +0300

    added method implementations

--- lib_dsu/dsu.h
+++ lib_dsu/dsu.h
@@ -1,7 +1,11 @@
+#include "iostream"
+#include "stdexcept"
+#include "../lib_dmassive/TDmassive.h"
+
 class DSU {
 	int _size;
-	int* _parent;
-	int* _rank;
+	TDMassive<int> _parent;
+	TDMassive<int> _rank;
 public:
 	DSU(int size = 0);
 	~DSU();
@@ -10,3 +14,44 @@ public:
 	void uni(int first, int second);
 	void clear();
 };
+
+DSU::DSU(int size = 0) {
+	_size = size;
+	_parent = TDMassive<int>(_size);
+	for (size_t i = 0; i < _size; i++) {
+		_parent[i] = i+1;
+	}
+	_rank = TDMassive<int>(_size, 0);
+}
+
+DSU::~DSU() {}
+
+void DSU::make_set(int elem) {
+	if (elem > _size + 1) {
+		throw std::logic_error("incorrect addition of a set");
+	}
+	_size++;
+	_parent[elem - 1] = elem;
+}
+
+int DSU::find(int elem) {
+	if (elem <= 0 || elem > _size) {
+		throw std::logic_error("Input Error: ...\n");
+	}
+	if (_parent[elem-1] == elem) {
+		return elem;
+	}
+	return find(_parent[elem-1]);
+}
+
+void DSU::uni(int first, int second) {
+	int top1 = find(first);
+	int top2 = find(second);
+	_parent[top2 - 1] = top1;
+}
+
+void DSU::clear() {
+	_size = 0;
+	_parent.clear();
+	_rank.clear();
+}
