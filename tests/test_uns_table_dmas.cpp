3927f3978a727c479e0504787f2f6e511124b9ab
SafronovMaks1m <safranov01@list.ru>
2025-03-08 20:31:37 +0300

    updating method

--- tests/test_uns_table_dmas.cpp
+++ tests/test_uns_table_dmas.cpp
@@ -1,70 +1,70 @@
-//#include <gtest.h>
-//#include "..//lib_pair/TPair.h"
-//#include "..//lib_dmassive/TDmassive.h"
-//#include "..//lib_unsort_table_dmassive/unsort_table_dmassive.h"
-//
-//TEST(TestUnsort_Table, check_func_fin) {
+#include <gtest.h>
+#include "..//lib_pair/TPair.h"
+#include "..//lib_dmassive/TDmassive.h"
+#include "..//lib_unsort_table_dmassive/unsort_table_dmassive.h"
+
+TEST(TestUnsort_Table, check_func_fin) {
+	TDMassive<TPair<int, int>> massive;
+	for (size_t i = 0; i < 5; i++) {
+		massive.push_back(TPair<int, int>(i + 1, i));
+	}
+	Unsorted_Table_DMassive<int, int> table(massive);
+	ASSERT_EQ(table.find(1), 0);
+	ASSERT_EQ(table.find(2), 1);
+	ASSERT_EQ(table.find(3), 2);
+	ASSERT_EQ(table.find(4), 3);
+	ASSERT_EQ(table.find(5), 4);
+}
+
+TEST(TestUnsort_Table_DMassive, check_func_insert1) {
+	TDMassive<TPair<int, int>> massive;
+	for (size_t i = 0; i < 5; i++) {
+		massive.push_back(TPair<int, int>(i + 1, i));
+	}
+	Unsorted_Table_DMassive<int, int> table(massive);
+	table.insert(10);
+	table.insert(11);
+	table.insert(12);
+	ASSERT_EQ(table.find(15), 10);
+	ASSERT_EQ(table.find(17), 11);
+	ASSERT_EQ(table.find(19), 12);
+}
+
+TEST(TestUnsort_Table_DMassive, check_func_insert2) {
+	Unsorted_Table_DMassive<int, int> table;
+	table.insert(1, 3);
+	table.insert(3, 5);
+	table.insert(2, 7);
+	ASSERT_EQ(table.find(1), 3);
+	ASSERT_EQ(table.find(3), 5);
+	ASSERT_EQ(table.find(2), 7);
+}
+
+TEST(TestUnsort_Table_DMassive, check_func_insert_throw) {
+	Unsorted_Table_DMassive<int, int> table;
+	table.insert(3, 5);
+	ASSERT_ANY_THROW(table.insert(3, 7));
+}
+
+//TEST(TestUnsort_Table_DMassive, check_func_erase) {
 //	TDMassive<TPair<int, int>> massive;
 //	for (size_t i = 0; i < 5; i++) {
 //		massive.push_back(TPair<int, int>(i + 1, i));
 //	}
 //	Unsorted_Table_DMassive<int, int> table(massive);
-//	ASSERT_EQ(table.find(1), 0);
-//	ASSERT_EQ(table.find(2), 1);
 //	ASSERT_EQ(table.find(3), 2);
-//	ASSERT_EQ(table.find(4), 3);
-//	ASSERT_EQ(table.find(5), 4);
-//}
-//
-//TEST(TestUnsort_Table_DMassive, check_func_insert1) {
-//	TDMassive<TPair<int, int>> massive;
+//	table.erase(3);
+//	ASSERT_ANY_THROW(table.find(3));
+//} ????????????????????
+
+//TEST(TestUnsort_Table, check_func_staples) {
+//	Tlist<TPair<int, int>> list;
 //	for (size_t i = 0; i < 5; i++) {
-//		massive.push_back(TPair<int, int>(i + 1, i));
+//		list.push_back(TPair<int, int>(i + 1, i));
 //	}
-//	Unsorted_Table_DMassive<int, int> table(massive);
-//	table.insert(10);
-//	table.insert(11);
-//	table.insert(12);
-//	ASSERT_EQ(table.find(15), 10);
-//	ASSERT_EQ(table.find(17), 11);
-//	ASSERT_EQ(table.find(19), 12);
+//	Unsorted_Table<int, int> table(list);
+//	ASSERT_EQ(table[1], 0);
+//	ASSERT_EQ(table[3], 2);
+//	table[3] = 7;
+//	ASSERT_EQ(table[3], 7);
 //}
-//
-//TEST(TestUnsort_Table_DMassive, check_func_insert2) {
-//	Unsorted_Table_DMassive<int, int> table;
-//	table.insert(1, 3);
-//	table.insert(3, 5);
-//	table.insert(2, 7);
-//	ASSERT_EQ(table.find(1), 3);
-//	ASSERT_EQ(table.find(3), 5);
-//	ASSERT_EQ(table.find(2), 7);
-//}
-//
-//TEST(TestUnsort_Table_DMassive, check_func_insert_throw) {
-//	Unsorted_Table_DMassive<int, int> table;
-//	table.insert(3, 5);
-//	ASSERT_ANY_THROW(table.insert(3, 7));
-//}
-//
-////TEST(TestUnsort_Table_DMassive, check_func_erase) {
-////	TDMassive<TPair<int, int>> massive;
-////	for (size_t i = 0; i < 5; i++) {
-////		massive.push_back(TPair<int, int>(i + 1, i));
-////	}
-////	Unsorted_Table_DMassive<int, int> table(massive);
-////	ASSERT_EQ(table.find(3), 2);
-////	table.erase(3);
-////	ASSERT_ANY_THROW(table.find(3));
-////} ????????????????????
-//
-////TEST(TestUnsort_Table, check_func_staples) {
-////	Tlist<TPair<int, int>> list;
-////	for (size_t i = 0; i < 5; i++) {
-////		list.push_back(TPair<int, int>(i + 1, i));
-////	}
-////	Unsorted_Table<int, int> table(list);
-////	ASSERT_EQ(table[1], 0);
-////	ASSERT_EQ(table[3], 2);
-////	table[3] = 7;
-////	ASSERT_EQ(table[3], 7);
-////}
